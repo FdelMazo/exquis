2534c375d9950d3b35f11bc986d66c2a21d037ab
Federico del Mazo <fdelmazo@fi.uba.ar>
2024-11-28 14:14:41 -0300

    Add favicon and title

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,10 @@ import { Providers } from "./providers";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>Los Años 20 | Exquis</title>
+      <link rel="shortcut icon" href="/static/favicon.png"></link>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <body
         style={{
           backgroundImage: "url('static/texture.jpg')",
